af52bf8e15a51d32c3d7e4d58cfc5cf02bc63243
jcarlos <jcpablo10@gmail.com>
2020-12-18 11:34:15 -0600

    home action buttons

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Hero  from '../Components/Hero'
+import Button from '../Components/Inputs/Button'
 import '../Components/Styles/Home.scss'
 
 const Home = () => {
     return (
-        <div className="home__container">
-            <Hero />
-        </div>
+        <>
+            <div className="homemenu__container">
+                <ul className="homemenu__list">
+                    <li className="homemenu__item">
+                        <Button textButton="Iniciar sesión" />
+                    </li>
+                    <li className="homemenu__item">
+                        <a href="#" className="link">Comunity</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="home__container">
+                <Hero />
+            </div>
+        </>
     )
 }
 
