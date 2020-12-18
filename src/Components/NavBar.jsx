7c5726779452f99777989bc286a0bf6a112ebff8
jcarlos <jcpablo10@gmail.com>
2020-12-18 10:23:20 -0600

    NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import NavBarLogo from '../assets/logo-horiz.svg'
+import '../Components/Styles/NavBar.scss'
 
 const NavBar = () => {
     return (
         <nav>
-
+            <div className="logo__container">
+                <img src={NavBarLogo} alt=""/>
+            </div>
+            <div className="useractions__container">
+                <span className="username">
+                    [Usuario]
+                </span>
+            </div>
         </nav>
     )
 }
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../App.scss'
+import NavBar from '../Components/NavBar'
 import Login from '../Pages/Login'
 
 function App() {
   return (
     <div>
+      <NavBar></NavBar>
       <Login />
     </div>
   )
