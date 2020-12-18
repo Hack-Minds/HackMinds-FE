dfc6ad1d5c48016102f247cdd387e8c7372fd239
jcarlos <jcpablo10@gmail.com>
2020-12-18 11:07:48 -0600

    hero

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero  from '../Components/Hero'
+import '../Components/Styles/Home.scss'
+
+const Home = () => {
+    return (
+        <div className="home__container">
+            <Hero />
+        </div>
+    )
+}
+
+export default Home
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import '../App.scss'
 import NavBar from '../Components/NavBar'
 import Login from '../Pages/Login'
+import Home from '../Pages/Home'
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Home />
       <Login />
     </div>
   )
