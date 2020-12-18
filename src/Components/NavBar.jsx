b00237af89b6578a13494d8d60c7eb6c86c3a74e
jcarlos <jcpablo10@gmail.com>
2020-12-18 10:00:30 -0600

    WIP navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBarLogo from '../assets/logo-horiz.svg'
+
+const NavBar = () => {
+    return (
+        <nav>
+
+        </nav>
+    )
+}
+
+export default NavBar
