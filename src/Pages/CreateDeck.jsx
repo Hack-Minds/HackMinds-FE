da662e3bdd506deee0507b4da8c1958db5463aa3
jcarlos <jcpablo10@gmail.com>
2020-12-18 20:19:25 -0600

    get user decks and create deck

--- src/Pages/CreateDeck.jsx
+++ src/Pages/CreateDeck.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
 import Button from '../components/Inputs/Button'
+import { useForm } from 'react-hook-form'
+import Input from '../Components/Inputs/Input'
 import '../components/Styles/CreateDeck.scss'
 
 function CreateDeck() {
+
+  const user = sessionStorage.getItem('Usuario')
+  const token = sessionStorage.getItem('Token')
+
+  const { register, handleSubmit } = useForm()
+  
+  const handleNewDeck = (data, e) => {
+    console.log(data)
+    fetch('http://3.129.13.14:8000/decks/', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Authorization': 'Token ' + token,
+        'Content-type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(res => console.log(res))
+
+    e.target.reset()
+  }
+
   return (
     <div className='createDeckContainer'>
       <div className='createDeck'>
         <h3>Crea un nuevo deck</h3>
-        <form>
-          <div>
-            <label>Tema</label>
-            <input />
-          </div>
-          <div>
-            <label>Descripción</label>
-            <input />
-          </div>
-          <Button textButton='Añadir' />
-        </form>
+        <form onSubmit={handleSubmit(handleNewDeck)}>
+        <input 
+          type="text" 
+          placeholder="Nombre" 
+          name="deck_name" 
+          ref={register}
+        />
+        <input 
+          type="textarea" 
+          placeholder="Descripcion" 
+          name="description" 
+          ref={register}
+        />
+        <button type="submit">Añadir</button>
+      </form>
       </div>
     </div>
   )
--- src/Pages/DashboardUser.jsx
+++ src/Pages/DashboardUser.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import NavBar from '../components/NavBar'
+import CreateDeck from '../Pages/CreateDeck'
+
 
 function DashboardUser() {
+  
   const user = sessionStorage.getItem('Usuario')
+  const token = sessionStorage.getItem('Token')
+
+  const [userDecks, setDecks] = useState([])
+
+     useEffect(() => {
+          fetch('http://3.129.13.14:8000/decks/',{
+          method: 'GET',
+          headers: {
+            'Authorization': 'Token ' + token,
+            'Content-type': 'application/json'
+          }
+        })
+        .then(response => response.json())
+        .then(data => setDecks(data.results))
+    },[])
 
   return (
     <div>
       <NavBar />
       DashboardUser
       <h4>{user}</h4>
+      {
+        userDecks && userDecks.map((item)=>{
+          console.log(item)
+        })
+      }
+      <CreateDeck></CreateDeck>
+      
     </div>
   )
 }
